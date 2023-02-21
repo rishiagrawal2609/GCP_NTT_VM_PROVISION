d7b8ba9edb0584caf70015a4e2eaad6698d1e3e8
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-02-21 18:09:52 +0530

    updated main.tf,var.tf,output.tf

--- main.tf
+++ main.tf
@@ -6,127 +6,51 @@ terraform {
     }
   }
 }
-
-resource "google_compute_network" "main" {
-  name = "myVmNet"
-  description = "Network for the VM"
-  auto_create_subnetworks = false
-  routing_mode = "GLOBAL"
-  mtu = 1460
-  enable_ula_internal_ipv6 = false 
-  internal_ipv6_range = ""
-  project = "cs-internal-cust-suc-cloudops"
-  delete_default_routes_on_create = false
-}
-resource "google_compute_subnetwork" "main" {
-  name = "subnet1"
-  ip_cidr_range = ""
-  network = "myVmNet"
-  description = "my subnet"
-  secondary_ip_range {
-    range_name = "secondary IP"
-    ip_cidr_range = ""
-  }
-  private_ip_google_access = true
-  private_ipv6_google_access = false
-  region = "us-central"
-  stack_type = "IPV4_IPV6"
-  ipv6_access_type = "INTERNAL"
-  project = "cs-internal-cust-suc-cloudops"
-}
-
 resource "google_compute_instance" "main" {
-  name = "vmname"
-  machine_type = "e2.medium"
-  zone = us-central1-a
-  tags = ["bastion"]
+  name = var.Vmname
+  machine_type = var.machine_type
+  zone = var.zone
+  tags = var.tags
   boot_disk {
-    auto_delete = true
-    device_name = "vmdisk"
-    mode = "READ_WRITE"
-    source = null
+    initialize_params {
+      image = var.OSimage
+      size = var.diskSize
+    }
+    auto_delete = var.auto_delete
+    device_name = var.diskName
+    mode = var.diskMode
+    source = var.source
   }
   network_interface {
-    network = "myVmNet"
-    subnetwork = "subnet1"
-    network_ip = null
+    network = var.myVmNet
+    subnetwork = var.mySubNet
+    subnetwork_project = var.subnetwork_project
+    network_ip = var.networkip
     access_config {
-      nat_ip = ""
-      public_ptr_domain_name = ""
-      network_tier = ""
-    }
-    ipv6_access_config {
-      public_ptr_domain_name = ""
-      network_tier = "STANDARD"
+      nat_ip = var.nat_ip
     }
     alias_ip_range {
-      public_ptr_domain_name = ""
-      network_tier = "STANDARD"
+      ip_cidr_range = var.ip_cidr_range_alias
     }
   }
-  allow_stopping_for_update = false
-  attached_disk {
-    source = ""
-    device_name = "my-device"
-
-  }
-  can_ip_forward = true
-  description = "Launch the VM"
-  desired_status = "RUNNING"
-  deletion_protection = false
-  hostname = "compute.example.com"
-  guest_accelerator {
-    type = null
-    count = null
-  }
-  labels = {
-    "key" = "value"
-  }
-  metadata = {
-    "key" = "value"
-  }
-  metadata_startup_script = null #give/path/to/script
-  project = "cs-internal-cust-suc-cloudops"
-  scheduling {
-    preemptible = false
-    on_host_maintenance = "MIGRATE"
-    automatic_restart = false
-    node_affinities {
-      key = "mylable"
-      operator = "IN"
-      values = [ "" ]
-    }
-    min_node_cpus = ""
-    provisioning_model = "STANDARD"
-    instance_termination_action = "STOP"
-  }
-  scratch_disk {
-    interface = "SCSI"
-  }
-  service_account {
-    email = ""
-    scope = ""
-  }
-  shielded_instance_config {
-    enable_secure_boot = null
-    enable_integrity_monitoring = null
-    enable_vtpm = null
-  }
-  enable_display = true
-  resource_policies = ""
-  reservation_affinity {
-    type = ""
-    specific_reservation = [""]
-  }
-
-  confidential_instance_config {
-    enable_confidential_compute = null
-  }
-
-  advanced_machine_features {
-    enable_nested_virtualization = false
-    threads_per_core = 1
-    visible_core_count = null
-  }
+  can_ip_forward = var.can_ip_forward
+  description = var.description
+  desired_status = var.desired_status
+  deletion_protection = var.deletion_protection
+  hostname = var.hostname
+  labels = var.labels
+  metadata = var.metadata
+  metadata_startup_script = var.metadata_startup_script
+  project = var.project
   
+  # service_account {
+  #   email = ""
+  #   scope = ""
+  # }
+  shielded_instance_config {
+    enable_secure_boot = var.enable_secure_boot
+    enable_integrity_monitoring = var.enable_integrity_monitoring
+    enable_vtpm = var.enable_vtpm
+  }
+
 }
--- output.tf
+++ output.tf
@@ -26,30 +26,13 @@ output "CPUPlatform" {
   value = google_compute_instance.main.cpu_platform
 }
 
-output "ipv6AccessType" {
-    value = google_compute_instance.main.ipv6_access_type
-}
+
 
 output "InternalIp" {
     value = google_compute_instance.main.network_interface.0.network_ip
 }
 
-output "natIp" {
-    value = google_compute_instance.main.network_interface.0.nat_ip
-}
 
-output "externalIpv6" {
-    value = google_compute_instance.main.0.ipv6_access_config.0.external_ipv6
-}
 
-output "VPCNetworkId" {
-  value = google_compute_network.main.id
-}
 
-output "ipv4Gateway" {
-    value = google_compute_network.main.gateway_ipv4
-}
 
-output "VpcSelfLink" {
-    value = google_compute_network.main.self_link
-}
